2c5386728e736c5b03372ad8358c664791ac54a8
Mayank Agarwal <109900878+Mayankagr2022@users.noreply.github.com>
2024-09-22 12:17:52 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://mynkagrwl1995:NewMayank%40123@cluster1.xo9ze.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {
+    await mongoose.connect('mongodb:localhost:5500', {
       dbName:"blog"
     });
     console.log('MongoDB Connected');
